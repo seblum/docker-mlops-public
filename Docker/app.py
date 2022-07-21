17c1ffb004cf67331662c89f67579c3239c18d59
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2022-07-21 21:13:47 +0200

    added index.html to flask docker image

--- Docker/app.py
+++ Docker/app.py
@@ -1,14 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome!"
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
