330c9a3cae62b093cbc3244316ab212f0c373e5d
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2022-07-23 23:27:18 +0200

    modified github actions

--- Docker/app.py
+++ Docker/app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
+# @app.route('/how are you')
+# def hello():
+#     return 'I am good, how about you?'
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port='5000')
