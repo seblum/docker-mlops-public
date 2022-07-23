e861e16eac39a2667ef484871ba19dc366f725f2
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2022-07-23 23:24:59 +0200

    removed git tag for test purposes

--- Docker/app.py
+++ Docker/app.py
@@ -13,5 +13,4 @@ def hello():
     return 'I am good, how about you?'
 
 if __name__ == "__main__":
-    #port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port='5000')
