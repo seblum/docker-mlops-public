03ec55b85b95ccc4c5b7a1ea6660b9b9c5d8fde1
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2022-08-04 18:12:10 +0200

    frontend flask image

--- frontend_flask/app.py
+++ frontend_flask/app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    IP = "0.0.0.0"
-    PORT = "5001"
+    IP = "flask_base"
+    PORT = "5000"
     r = requests.get(f"http://{IP}:{PORT}/data")
     data = r.json()
     return render_template('index.html', d=data)
--- frontend_streamlit/app.py
+++ frontend_streamlit/app.py
@@ -5,9 +5,11 @@ import pandas as pd
 def main():
     st.title("Data from Backend")
 
-    r = requests.get("http://0.0.0.0:5001/data")
+    #IP = "0.0.0.0"
+    IP = "flask_base"
+    r = requests.get(f"http://{IP}:5000/data")
     data = r.json()
-    #st.json(data)
+    st.json(data)
 
     df = pd.DataFrame.from_dict(data, orient='index')
     st.table(df)
