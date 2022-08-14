70972a8d4e401fc8460e4d39345dbaff1406e164
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2022-08-14 22:08:58 +0200

    commented kill_in_seconds in frontend

--- frontend-streamlit/app.py
+++ frontend-streamlit/app.py
@@ -8,12 +8,12 @@ import sys
 def main():
 
     DB_SERVICE = os.environ.get("DB_SERVICE")
-    KILL_IN_SECONDS = os.environ.get("KILL_IN_SECONDS")
+    # KILL_IN_SECONDS = os.environ.get("KILL_IN_SECONDS")
 
     with st.sidebar:
         st.subheader("Environment variables")
         st.text(f"DB_SERVICE: {DB_SERVICE}")
-        st.text(f"KILL_IN_SECONDS: {KILL_IN_SECONDS}")
+        # st.text(f"KILL_IN_SECONDS: {KILL_IN_SECONDS}")
 
     st.title("Frontend Deployment")
     #DB_SERVICE = "flask_base"
@@ -27,22 +27,22 @@ def main():
         df = pd.DataFrame.from_dict(data, orient='index')
         st.table(df)
 
-    if KILL_IN_SECONDS is not None:
-        st.subheader("Kill in seconds")
-        st.text(f"exit process in {KILL_IN_SECONDS}")
+    # if KILL_IN_SECONDS is not None:
+    #     st.subheader("Kill in seconds")
+    #     st.text(f"exit process in {KILL_IN_SECONDS}")
 
-        KILL_IN_SECONDS = int(KILL_IN_SECONDS)
-        my_bar = st.progress(0)
+    #     KILL_IN_SECONDS = int(KILL_IN_SECONDS)
+    #     my_bar = st.progress(0)
 
-        step_size = KILL_IN_SECONDS/100
-        my_bar.progress(1)
-        for percent_complete in range(100):
-            sleep(step_size)
-            my_bar.progress(percent_complete + 1)
-        st.warning("Streamlit App killed")
-        sleep(1)
-        # streamlit catches sys.exit()
-        os._exit(1)
+    #     step_size = KILL_IN_SECONDS/100
+    #     my_bar.progress(1)
+    #     for percent_complete in range(100):
+    #         sleep(step_size)
+    #         my_bar.progress(percent_complete + 1)
+    #     st.warning("Streamlit App killed")
+    #     sleep(1)
+    #     # streamlit catches sys.exit()
+    #     os._exit(1)
 
 if __name__ == "__main__":
     main()
